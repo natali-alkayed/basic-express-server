36c353a60a75230034159afdfcd46b513865383d
natali-alkayed <nataly2000alkayed@gmail.com>
2023-06-07 16:43:06 +0000

    done

--- __test__/server.test.js
+++ __test__/server.test.js
@@ -9,17 +9,4 @@ describe("API Server ", () => {
         expect(response.status).toEqual(200);
         expect(response.text).toEqual("hello from home route");
     });
-    it("handle 404 error", async () => {
-        const response = await request.get('/abd');
-        expect(response.status).toEqual(404);
-    });
-    test("handle server error", async () => {
-        const response = await request.get('/bad');
-        expect(response.status).toEqual(500);
-    });
-    it("testing /data", async () => {
-        const response = await request.get('/data');
-        expect(response.status).toEqual(200);
-        expect(typeof response.body).toEqual("object");
-    })
 });
